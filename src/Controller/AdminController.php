7f994976b41dd7740ea79657cebe2b65911e1293
SamAdeSan <samadesan@alu.edu.gva.es>
2025-12-11 10:44:07 +0100

    Actualizado estilos y hecho la mitad de Zona de administracion

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Category;
+use App\Form\CategoryFormType;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+#[IsGranted('ROLE_ADMIN')]
+final class AdminController extends AbstractController
+{
+    #[Route('/admin/images', name: 'app_images')]
+    public function images(): Response
+    {
+        return $this->render('admin/images.html.twig', []);
+    }
+    #[Route('/admin/categories', name: 'app_categories')]
+    public function categories(ManagerRegistry $doctrine, Request $request): Response
+    {
+        $repositorio = $doctrine->getRepository(Category::class);
+
+        $categories = $repositorio->findAll();
+
+        $category = new Category();
+        $form = $this->createForm(CategoryFormType::class, $category);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $category = $form->getData();
+            $entityManager = $doctrine->getManager();
+            $entityManager->persist($category);
+            $entityManager->flush();
+        }
+        return $this->render('admin/categories.html.twig', array(
+            'form' => $form->createView(),
+            'categories' => $categories
+        ));
+    }
+}
